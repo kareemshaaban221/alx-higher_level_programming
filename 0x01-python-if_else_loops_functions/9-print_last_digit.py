0e2cecd65eac1fac2372d42d9a3112ffe139a930
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-06 17:52:33 +0300

    test

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number2 = 0
+    if number < 0:
+        number2 = (10 - (number % 10)) * -1
+    else:
+        number2 = number % 10
+    print("{:d}".format(number2), end='')
