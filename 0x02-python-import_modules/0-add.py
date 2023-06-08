01e6c88ddc4b8280dbf56a98e9b864d3b6a424a8
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-09 00:30:02 +0300

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0 as alias
 a = 1
 b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, alias.add(a, b)))
