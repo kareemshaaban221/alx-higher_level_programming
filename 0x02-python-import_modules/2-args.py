0ba1d0f2e0fb0014f582c27ea2ba1f3ba53e38ef
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-09 01:05:19 +0300

    test

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-length = len(argv) - 1
-if length <= 0:
-    print("0 arguments.")
-    exit
-elif length == 1:
-    print("1 argument:")
-else:
-    print("{:d} arguments:".format(length))
-i = 0
-for val in argv:
-    if i != 0:
-        print("{:d}: {:s}".format(i, val))
-    i += 1
+if __name__ == '__main__':
+    from sys import argv
+    length = len(argv) - 1
+    if length <= 0:
+        print("0 arguments.")
+        exit
+    elif length == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(length))
+    i = 0
+    for val in argv:
+        if i != 0:
+            print("{:d}: {:s}".format(i, val))
+        i += 1
