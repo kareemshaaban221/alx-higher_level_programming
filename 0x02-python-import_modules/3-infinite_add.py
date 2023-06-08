52fd8e1241e59abe19775a5e881b5402eb33e1c9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-09 01:13:08 +0300

    test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     from sys import argv
+    i = 0
     res = 0
     for val in argv:
-        res += int(val)
+        if i != 0:
+            res += int(val)
+        i += 1
     print("{}".format(res))
