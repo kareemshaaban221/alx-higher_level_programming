f9a0354d8209ac5c69de4a6e614a2d8fd572580f
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-12 21:53:07 +0300

    test

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -12,11 +12,3 @@ def new_in_list(my_list, idx, element):
             res.append(element)
         i += 1
     return res
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
