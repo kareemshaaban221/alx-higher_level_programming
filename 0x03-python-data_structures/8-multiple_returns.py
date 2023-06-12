b4f8c7a58568a168285a83cbe46a4db7fbc27901
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-13 00:41:58 +0300

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return (0, 'None')
-    return (len(sentence), sentence[0])
+    length = len(sentence)
+    c = sentence[0] if length > 0 else "None"
+    tup = length, c
+    return(tup)
