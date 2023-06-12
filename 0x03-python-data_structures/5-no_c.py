64c9518ce7177a584f91c2191ac06b2fb3fe52e9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-12 22:00:04 +0300

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
+    if my_string is None:
+        return None
     res = ''
     for c in my_string:
         if c == 'c' or c == 'C':
             continue
-        res.append(c)
+        res += c
     return res
