8027d4845a7b517db553eaf9781c9049e17390f5
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-15 01:26:13 +0300

    test

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     res = {}
-    for k, v in a_dictionary:
-        if v % 2 == 0:
-            res[k] = v
+    for k in a_dictionary:
+        if a_dictionary[k] % 2 == 0:
+            res[k] = a_dictionary[k]
     return res
-
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
