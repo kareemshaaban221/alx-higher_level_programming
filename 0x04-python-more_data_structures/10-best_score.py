97e7f55b29249859a35827435fcc972314cdce93
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-06-15 01:34:30 +0300

    test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
     res = None
     for k in a_dictionary:
         if res is None:
