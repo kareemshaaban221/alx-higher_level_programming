67050829c326f9a43ea36a8ccfb6e66c52ffbf30
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-11 21:35:49 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """
+    Add two integers together
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
