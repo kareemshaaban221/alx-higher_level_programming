64b1f8f2c8c68482fb5a8665d41f1c7d19c64b12
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-11 21:25:00 +0300

    test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """
+        add_integer(a, b)
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    a, b = int(a), int(b)
+    return a + b
