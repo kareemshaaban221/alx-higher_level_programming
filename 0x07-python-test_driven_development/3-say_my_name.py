9bac44b04f75aca81fd8a3a1eec3066838a49f87
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-11 22:21:40 +0300

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Module say_my_name
+Prints a given first name and last name.
+"""
+
 def say_my_name(first_name, last_name=""):
+    """Prints a string with <first_name>
+    and <last_name>.
+    """
+    
     errorMessage = "first_name must be a string"
     errorMessage2 = "last_name must be a string"
     if type(first_name) != str:
