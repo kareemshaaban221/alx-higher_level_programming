13e25a67dcd402a63d6628c6d4b2cc9ef15949a9
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-11 22:19:08 +0300

    test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    errorMessage = "first_name must be a string or last_name must be a string"
     if type(first_name) != str and type(last_name) != str:
-        raise TypeError("first_name must be a string or last_name must be a string")
+        raise TypeError(errorMessage)
     print("My name is {} {}".format(first_name, last_name), end="")
