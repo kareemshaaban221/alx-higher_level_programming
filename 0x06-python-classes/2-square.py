92d4560c75e767a70f270346ce1fdb1bf1a3d5e0
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-14 20:36:42 +0300

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
     __size = None
 
     def __init__(self, size=0):
-        if type(size) != type(int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
