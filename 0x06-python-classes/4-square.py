2ead8ae4ae36f3d7e69d1d2f3d32781ce3d1809e
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-14 20:42:43 +0300

    test

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This Module is for define Square Class
+"""
+
+
+class Square:
+    """
+    Square Class
+    """
+
+    __size = None
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
