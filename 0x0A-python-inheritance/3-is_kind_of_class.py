ddb89c7f916341c642dc126921c91ba4fd51e12f
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 01:53:43 +0200

    test

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return isinstance(obj, a_class)
