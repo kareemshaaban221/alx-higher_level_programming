25a8550e57743d866c648e44412bb88d4f59ae12
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 01:58:55 +0200

    test

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+class BaseGeometry():
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+    """
+    pass
