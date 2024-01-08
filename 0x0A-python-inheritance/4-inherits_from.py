b5487f476612e35ea4f7060ac3df5de35967fe53
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 01:54:52 +0200

    test

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return issubclass(type(obj), a_class)
