7238b7e4075a1c708520c6fb5439a7969e2c4518
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 01:44:23 +0200

    test

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,13 @@ Module with class MyList
 
 
 def is_same_class(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return type(obj) == a_class
