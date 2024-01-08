3e4f28fa894134e17be48265a7d2c8133c37a2e5
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 01:39:51 +0200

    test

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+def is_same_class(obj, a_class):
+    return isinstance(obj, a_class)
