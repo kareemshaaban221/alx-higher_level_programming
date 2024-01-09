9c36be3194f147c3b869faf07e40dddb09ef92b8
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-10 01:26:43 +0200

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,14 +3,13 @@
 Reads from a file and prints.
 """
 import sys
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-args = sys.argv[1:]
 try:
-    current = list(load_from_json_file('add_item.json'))
-except Exception as e:
-    current = []
+    items = list(load_from_json_file('add_item.json'))
+except FileNotFoundError:
+    items = []
 finally:
-    for i in args:
-        current.append(i)
-    save_to_json_file(current, 'add_item.json')
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
