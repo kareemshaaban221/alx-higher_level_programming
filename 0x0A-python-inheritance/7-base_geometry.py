e0a9b7263dd8c99d436db0d516aba9632cfe7f15
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 02:10:59 +0200

    test

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+class BaseGeometry():
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+    """
+    def area(self):
+        """_summary_
+        """
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        """_summary_
+
+        Args:
+            name (_type_): _description_
+            value (_type_): _description_
+        """
+        if type(value) is not int:
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
