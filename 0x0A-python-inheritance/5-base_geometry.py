9b04d1c235ce096ddafb97fe177ae0710ff5ed8f
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 02:01:14 +0200

    test

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python3
-"""
-===========================
-Module with class MyList
-===========================
-"""
-
-
-class BaseGeometry():
-    """_summary_
-
-    Args:
-        list (_type_): _description_
-    """
-    pass
+c
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class MyList
+===========================
+"""
+
+
+class BaseGeometry():
+    """_summary_
+
+    Args:
+        list (_type_): _description_
+    """
+    def area(self):
+        """_summary_
+        """
+        raise Exception('area() is not implemented')
