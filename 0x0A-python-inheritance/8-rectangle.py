0d099f822144b0ff4cefa726e788e705b2e94c5a
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-09 02:27:52 +0200

    test

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 ===========================
 Module with class MyList
@@ -6,13 +7,19 @@ Module with class MyList
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """_summary_
     """
     __width = None
     __height = None
+
     def __init__(self, width, height):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+        """
         self.integer_validator('width', width)
         self.integer_validator('height', height)
         self.__width = width
