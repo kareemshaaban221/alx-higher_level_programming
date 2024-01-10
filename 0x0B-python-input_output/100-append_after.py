b21b3111d7cc034e92605282af34cef6b96f4ad0
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-10 02:35:56 +0200

    test

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """_summary_
+
+    Args:
+        n (_type_): _description_
+    """
+    
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    new_content = ""
+    for line in lines:
+        new_content += line
+        if search_string in line:
+            new_content += new_string + '\n'
+    with open(filename, 'w') as f:
+        f.write(new_content)
