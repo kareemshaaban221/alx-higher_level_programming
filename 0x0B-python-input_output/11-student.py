18350070dd19f00f358a59f3f7eb0843dda03061
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-10 02:03:50 +0200

    test

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" My class module
+"""
+
+
+class Student:
+    """ My class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        di = self.__dict__
+        res = {}
+        if type(attrs) is list:
+            for k, v in di.items():
+                if k in attrs:
+                    res[k] = v
+        else:
+            return di
+        return res
+
+    def reload_from_json(self, json):
+        self.__dict__.clear()
+        for k, v in json.items():
+            self.__dict__[k] = v
