894d789ef641e7746df2f3342fb47b24033ef9ef
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-10 03:29:53 +0200

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,21 +7,31 @@ from sys import stdin
 dictstatus = {}
 totalsize = 0
 totalcount = 0
-for line in stdin:
-    split = line.split()
-    status = split[-2]
-    totalsize += int(split[-1])
-    if status in dictstatus.keys():
-        dictstatus[status] += 1
-    else:
-        dictstatus[status] = 1
-    totalcount += 1
-    if totalcount == 10:
-        sortme = sorted(dictstatus.keys())
-        print(f"File size: {totalsize}")
-        for keys in sortme:
-            print("{}: {}".format(keys, dictstatus[keys]))
-        totalcount = 0
-        totalsize = 0
-        dictstatus = {}
-        continue
+try:
+    for line in stdin:
+        split = line.split()
+        status = split[-2]
+        totalsize += int(split[-1])
+        if status in dictstatus.keys():
+            dictstatus[status] += 1
+        else:
+            dictstatus[status] = 1
+        totalcount += 1
+        if totalcount == 10:
+            sortme = sorted(dictstatus.keys())
+            print(f"File size: {totalsize}")
+            for keys in sortme:
+                print("{}: {}".format(keys, dictstatus[keys]))
+            totalcount = 0
+            totalsize = 0
+            dictstatus = {}
+            continue
+except KeyboardInterrupt as e:
+    sortme = sorted(dictstatus.keys())
+    print(f"File size: {totalsize}")
+    for keys in sortme:
+        print("{}: {}".format(keys, dictstatus[keys]))
+    totalcount = 0
+    totalsize = 0
+    dictstatus = {}
+    raise e
