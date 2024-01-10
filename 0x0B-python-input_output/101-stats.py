58257ebf4a3fd72842d45817dc449fba63e19d85
Kareem Shaban <kareemshaaban221gmail.com>
2024-01-10 03:22:51 +0200

    test

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,33 +4,22 @@
 from sys import stdin
 
 
-def print_stats(lines):
-    """sumary_line"""
-    statuscodes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                   '403': 0, '404': 0, '405': 0, '500': 0}
-    total_file_size = 0
-    for line in lines:
-        arr = str(line).split()
-        statuscode = arr[len(arr) - 2]
-        statuscodes[statuscode] += 1
-        filesize = arr[len(arr) - 1]
-        total_file_size += int(filesize)
-    print(f"File size: {total_file_size}")
-    for k, v in statuscodes.items():
-        if statuscodes[k] > 0:
-            print(f"{k}: {v}")
-
-lines = []
-i = 0
-while True:
-    try:
-        lines.append(stdin.readline())
-    except KeyboardInterrupt as e:
-        print_stats(lines)
-        break;
-    i += 1
-    if i % 10 == 0:
-        i = 0
-        print_stats(lines)
-        lines = []
-raise KeyboardInterrupt
+dictstatus = {}
+totalsize = 0
+totalcount = 0
+for line in stdin:
+    split = line.split()
+    status = split[-2]
+    totalsize += int(split[-1])
+    if status in dictstatus.keys():
+        dictstatus[status] += 1
+    else:
+        dictstatus[status] = 1
+    totalcount += 1
+    if totalcount == 10:
+        sortme = sorted(dictstatus.keys())
+        print("File size:", totalsize)
+        for keys in sortme:
+            print("{}: {}".format(keys, dictstatus[keys]))
+        totalcount = 0
+        continue
