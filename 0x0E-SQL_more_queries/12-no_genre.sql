3c6eb44e5cc4eaeda9e033aa8f5af0aa0fc19d49
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-02-16 04:30:55 +0200

    test

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- This is my comment
-SELECT a.title, b.genre_id
-FROM tv_shows AS a
-LEFT OUTER JOIN tv_show_genres AS b
-ON b.show_id = a.id
-ORDER BY a.title, b.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
