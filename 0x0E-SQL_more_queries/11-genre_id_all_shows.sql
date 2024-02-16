791d2a20c6827c43b53b06bf741999cf9fd4695a
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-02-16 04:08:30 +0200

    test

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- This is my comment
 SELECT a.title, b.genre_id
 FROM tv_shows AS a
-JOIN tv_show_genres AS b
+LEFT JOIN tv_show_genres AS b
 ON b.show_id = a.id
-ORDER BY (a.title, b.genre_id) ASC;
+ORDER BY a.title, b.genre_id ASC;
