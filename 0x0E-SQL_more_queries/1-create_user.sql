92b1e81549e85bab3dec34917c4e5967a9d64137
kareemshaaban221 <kareemshaaban221@gmail.com>
2024-02-16 03:20:58 +0200

    test

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- This is my comment
-CREATE USER user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES TO user_0d_1@localhost;
